351950e0076d0162d14cb0b1df9e162860ee0fff
mahendrasaikumargandham <mahendra_gandham@protonmail.com>
2022-01-28 20:21:28 +0530

    Added Background colors

--- src/Pages/Regulation/Regulation.js
+++ src/Pages/Regulation/Regulation.js
@@ -16,7 +16,35 @@ function Regulation() {
       
   return (
     <div className="regulation">
-        <button onClick={handleAuthenticaton}>Log out</button>
+        <button onClick={handleAuthenticaton}>LogOut</button>
+        <section>
+        <div className="container">
+            <div className="card">
+                <div className="content">
+                    <h2>R16</h2>
+                    <div className="contentBox">
+                        <span>Enter</span>
+                    </div>
+                </div>
+            </div>
+            <div className="card">
+                <div className="content">
+                    <h2>R19</h2>
+                    <div className="contentBox">
+                        <span>Enter</span>
+                    </div>
+                </div>
+            </div>
+            <div className="card">
+                <div className="content">
+                    <h2>R20</h2>
+                    <div className="contentBox">
+                        <span>Enter</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
     </div>
   );
 }
